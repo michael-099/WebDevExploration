e01fae56ed8f2bf19f6dcbb92cbb53db3efb10e3
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-03-13 23:28:36 +0300

    fetch usong react query

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { QueryClientProvider, QueryClient, QueryClient } from 'react-query';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import HomePage from "./components/homePage"
 import QuerySuperHeroes from "./components/querySuperHeroes";
 import SuperHeroesPage from './components/superHeroPage';
 
-const queryClient= new QueryClient()
+const queryClient = new QueryClient()
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-    {/* <QuerySuperHeroes /> */}
-      <SuperHeroesPage />
-    {/*  <HomePage /> */}
-      </QueryClientProvider>
+      <QuerySuperHeroes />
+      {/* <SuperHeroesPage /> */}
+      {/*  <HomePage /> */}
+    </QueryClientProvider>
 
-      );
+  );
 }
 
-   export default App;
+export default App;
--- src/components/querySuperHeroes.js
+++ src/components/querySuperHeroes.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
 
 const QuerySuperHeroes = () => {
-    return <div>
-        RQSuperHeroes
-    </div>
-}
+    const { isLoading, data, error } = useQuery('super-heroes', () => {
+        return axios.get('http://localhost:4000/superheros');
+    });
+
+    if (isLoading) {
+        return <h2>Loading ..</h2>;
+    }
+
+    if (error) {
+        return <h2>Error: {error.message}</h2>;
+    }
+
+    return (
+        <div>
+            {
+                data?.data.map(hero => {
+                    return <div key={hero.name}>{hero.name}</div>;
+                })
+            }
+        </div>
+    );
+};
 
 export default QuerySuperHeroes;
