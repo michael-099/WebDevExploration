e56f733afaedd5432c1feb1e2053cb06b6c63366
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-03-18 13:46:43 +0300

    fetch

--- src/Todo/demo.js
+++ src/Todo/demo.js
@@ -1,8 +1,23 @@
+import { useQuery, useMutation } from "react-query";
 
-function Todo(){
-    
-    return <div>
-        hello 
-    </div>
+
+const getData = async () => {
+    const response = await fetch('todo.json');
+    return await response.json()
+}
+function Todo() {
+    // return <p>hello</p>
+    const { data, isLoading, error } = useQuery("todo", getData);
+    if (isLoading) return <div> Loding the to do lists....</div>;
+    if (error) return <div>Error fetching data: {error.message}</div>
+    return (
+        <div>
+          {data.map((item) => (
+            <p key={item.title}> 
+              {item.todo}
+            </p>
+          ))}
+        </div>
+      );
 }
 export default Todo;
